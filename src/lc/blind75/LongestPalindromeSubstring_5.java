1102795fc3c67a9a251a767c7c579ae613e28cd5
Thanaphoom Babparn <tp.babparn@gmail.com>
2022-04-30 19:18:53 +0700

    Add sample medium label questions solutions * 11 * 49 * 5 * 3

---
+++ src/lc/blind75/LongestPalindromeSubstring_5.java
@@ -0,0 +1,38 @@
+package lc.blind75;
+
+public class LongestPalindromeSubstring_5 {
+
+  public String longestPalindrome(String s) {
+    if (s == null || s.length() < 2) {
+      return s;
+    }
+    int start = 0;
+    int end = 0;
+
+    for (int i = 0; i < s.length(); i++) {
+      // try on both case that might be happened
+      // assume odd length, try to expand palindrome as possible
+      int len1 = expandFromCenter(s, i, i);
+      // assume even length
+      int len2 = expandFromCenter(s, i, i + 1);
+      int longest = Integer.max(len1, len2);
+
+      if (longest > end - start) {
+        start = i - ((longest - 1) / 2);
+        end = i + (longest / 2);
+      }
+
+    }
+    return s.substring(start, end + 1);
+  }
+
+  private int expandFromCenter(String s, int left, int right) {
+    int l = left;
+    int r = right;
+    while (l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r)) {
+      l--;
+      r++;
+    }
+    return r - l - 1;
+  }
+}
