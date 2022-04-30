3e8ca0a9eb9649eb63eef8132f70e7dca9e1edf4
Thanaphoom Babparn <tp.babparn@gmail.com>
2022-04-30 19:18:26 +0700

    Add sample easy label questions solutions * 141 * 21 * 190 * 206 * 1 * 20

---
+++ src/lc/blind75/LinkedListCycle_141.java
@@ -0,0 +1,20 @@
+package lc.blind75;
+
+import lc.common.ListNode;
+
+public class LinkedListCycle_141 {
+
+  public boolean hasCycle(ListNode head) {
+    ListNode fast = head;
+    ListNode slow = head;
+    while (fast != null && fast.next != null) {
+      fast = fast.next.next;
+      slow = slow.next;
+      if (fast == slow) {
+        break;
+      }
+    }
+    return fast != null && fast.next != null;
+  }
+
+}
