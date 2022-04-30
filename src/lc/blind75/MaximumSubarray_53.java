d7c3e83b25ab33807057eb2e6c400957cf3c8c1e
Thanaphoom Babparn <tp.babparn@gmail.com>
2022-04-30 19:42:35 +0700

    Add 53 MaximumSubarray

---
+++ src/lc/blind75/MaximumSubarray_53.java
@@ -0,0 +1,21 @@
+package lc.blind75;
+
+public class MaximumSubarray_53 {
+
+  public int maxSubArray(int[] nums) {
+    if (nums == null || nums.length == 0) {
+      return 0;
+    }
+    if (nums.length == 1) {
+      return nums[0];
+    }
+    int maxSoFar = Integer.MIN_VALUE;
+    int currMax = 0;
+    for (int n : nums) {
+      // Kadane's Algorithms
+      currMax = Integer.max(currMax + n, n);
+      maxSoFar = Integer.max(maxSoFar, currMax);
+    }
+    return maxSoFar;
+  }
+}
