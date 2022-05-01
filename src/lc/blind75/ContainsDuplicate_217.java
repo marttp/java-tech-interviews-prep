ae48585313435154b50e74b9e1bb184af0aeac4e
Thanaphoom Babparn <tp.babparn@gmail.com>
2022-05-01 09:19:26 +0700

    [Easy] 217. Contains Duplicate

---
+++ src/lc/blind75/ContainsDuplicate_217.java
@@ -0,0 +1,17 @@
+package lc.blind75;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ContainsDuplicate_217 {
+
+  public boolean containsDuplicate(int[] nums) {
+    Set<Integer> set = new HashSet<>();
+    for (int n : nums) {
+      if (!set.add(n)) {
+        return true;
+      }
+    }
+    return false;
+  }
+}
