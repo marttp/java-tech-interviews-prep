90c08ee32134baf755c122dd135dfc74caed934c
Thanaphoom Babparn <tp.babparn@gmail.com>
2022-05-01 20:47:01 +0700

    [Medium] 105. Construct Binary Tree from Preorder and Inorder Traversal

---
+++ src/lc/blind75/medium/ConstructBinaryTreeFromPreorderAndInorderTraversal_105.java
@@ -0,0 +1,35 @@
+package lc.blind75.medium;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import lc.common.TreeNode;
+
+public class ConstructBinaryTreeFromPreorderAndInorderTraversal_105 {
+
+  public TreeNode buildTree(int[] preorder, int[] inorder) {
+    // Mapping element kv(numValue, index)
+    Map<Integer, Integer> map = IntStream.range(0, inorder.length)
+        .boxed()
+        .collect(Collectors.toMap((e) -> inorder[e], (e) -> e));
+    return helper(preorder, 0, preorder.length, inorder, 0, inorder.length, map);
+  }
+
+  private TreeNode helper(int[] preorder, int preorderStart, int preorderEnd, int[] inorder,
+      int inorderStart, int inorderEnd, Map<Integer, Integer> inorderMapToIndex) {
+    if (preorderStart >= preorderEnd || inorderStart >= inorderEnd) {
+      return null;
+    }
+    // Use Inorder as a traversal topology look like
+    int value = preorder[preorderStart];
+    int rootIndex = inorderMapToIndex.get(value);
+    int leftSubTreeSize = rootIndex - inorderStart;
+    return new TreeNode(value,
+        // left
+        helper(preorder, preorderStart + 1, preorderStart + 1 + leftSubTreeSize, inorder,
+            inorderStart, rootIndex, inorderMapToIndex),
+        // right
+        helper(preorder, preorderStart + 1 + leftSubTreeSize, preorderEnd, inorder, rootIndex + 1,
+            inorderEnd, inorderMapToIndex));
+  }
+}
