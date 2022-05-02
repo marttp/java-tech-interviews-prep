2b1f8c39518b2dfe4685406054e20048a9bac0eb
Thanaphoom Babparn <tp.babparn@gmail.com>
2022-05-02 14:28:20 +0700

    Minimum Spanning Tree - Prim (Greedy)

---
+++ src/additional/PrimAlgorithms.java
@@ -0,0 +1,34 @@
+package additional;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.PriorityQueue;
+import lc.common.Edge;
+
+public class PrimAlgorithms {
+
+  public int primAlgorithms(int n, List<Edge> edges) {
+    if (edges == null || edges.isEmpty()) {
+      return 0;
+    }
+    int size = n - 1; // Prim do (n - 1) times due to edge size
+    // Min Heap => For combined the shortest distance first
+    PriorityQueue<Edge> pq = new PriorityQueue<>(Comparator.comparingInt(e -> e.weight));
+    pq.addAll(edges);
+
+    boolean[] visited = new boolean[n];
+    Edge sourceNode = pq.peek();
+    visited[sourceNode.u] = true;
+    int cost = 0;
+
+    while (!pq.isEmpty() && size > 0) {
+      Edge edge = pq.poll();
+      if (!visited[edge.v]) {
+        cost += edge.weight;
+        size--;
+        visited[edge.v] = true;
+      }
+    }
+    return cost;
+  }
+}
