114c513bf606b27e3be520a4531675d7bdc2f732
Thanaphoom Babparn <tp.babparn@gmail.com>
2022-05-03 13:15:12 +0700

    [Easy] 637. Average of Levels in Binary Tree

---
+++ src/lc/pattern/treebfs/AvgOfLevelsInBinaryTree_637.java
@@ -0,0 +1,35 @@
+package lc.pattern.treebfs;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import lc.common.TreeNode;
+
+public class AvgOfLevelsInBinaryTree_637 {
+
+  public List<Double> averageOfLevels(TreeNode root) {
+    List<Double> result = new ArrayList<>();
+    if (root == null) {
+      return result;
+    }
+    Queue<TreeNode> q = new LinkedList<>();
+    q.add(root);
+    while (!q.isEmpty()) {
+      int size = q.size();
+      double sum = 0;
+      while (size-- > 0) {
+        var currNode = q.poll();
+        sum += currNode.val;
+        if (currNode.left != null) {
+          q.add(currNode.left);
+        }
+        if (currNode.right != null) {
+          q.add(currNode.right);
+        }
+      }
+      result.add(sum / size);
+    }
+    return result;
+  }
+}
