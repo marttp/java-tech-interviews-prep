43e934252c1eb907c562b6e216bcd4476c402ac3
Thanaphoom Babparn <tp.babparn@gmail.com>
2022-05-03 13:27:35 +0700

    [Easy] 111. Minimum Depth of Binary Tree

---
+++ src/lc/pattern/treebfs/MinimumDepthOfBinaryTree_111.java
@@ -0,0 +1,31 @@
+package lc.pattern.treebfs;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import lc.common.TreeNode;
+
+public class MinimumDepthOfBinaryTree_111 {
+
+  public int minDepth(TreeNode root) {
+    if (root == null) {
+      return 0;
+    }
+    Queue<TreeNode> q = new LinkedList<>();
+    int level = 0;
+    q.add(root);
+    while (!q.isEmpty()) {
+      int size = q.size();
+      while (size-- > 0) {
+        var node = q.poll();
+        if (node.left != null) {
+          q.add(node.left);
+        }
+        if (node.right != null) {
+          q.add(node.right);
+        }
+      }
+      level++;
+    }
+    return level;
+  }
+}
