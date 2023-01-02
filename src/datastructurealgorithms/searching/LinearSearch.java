a9c1a1d3a9bd21b4cd466cd8641ddb8ac89dd081
Thanaphoom Babparn <tp.babparn@gmail.com>
2023-01-02 18:09:33 +0900

    Adding Searching

---
+++ src/datastructurealgorithms/searching/LinearSearch.java
@@ -0,0 +1,13 @@
+package datastructurealgorithms.searching;
+
+public class LinearSearch {
+
+    public static int search(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
