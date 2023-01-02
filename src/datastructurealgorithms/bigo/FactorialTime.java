d91b29bc662b9c9be01d4c5d8cbcb734f2affca6
Thanaphoom Babparn <tp.babparn@gmail.com>
2023-01-02 17:57:13 +0900

    Big O - Adding Permutation n! Time

---
+++ src/datastructurealgorithms/bigo/FactorialTime.java
@@ -0,0 +1,12 @@
+package datastructurealgorithms.bigo;
+
+import lc.pattern.backtracking.Permutation_46;
+
+import java.util.List;
+
+public class FactorialTime {
+
+    public List<List<Integer>> permute(int[] nums) {
+        return new Permutation_46().permute(nums);
+    }
+}
