aeb23594e10c8d5329cb8ece441f154e27d21040
Thanaphoom Babparn <tp.babparn@gmail.com>
2023-01-03 11:56:26 +0900

    Adding tree traversal

---
+++ src/datastructurealgorithms/tree/Traversal.java
@@ -0,0 +1,35 @@
+package datastructurealgorithms.tree;
+
+import common.TreeNode;
+
+import java.util.List;
+
+public class Traversal {
+
+    private static void preorderTraversal(TreeNode root, List<Integer> result) {
+        if (root == null) {
+            return;
+        }
+        result.add(root.val);
+        preorderTraversal(root.left, result);
+        preorderTraversal(root.right, result);
+    }
+
+    private static void inorderTraversal(TreeNode root, List<Integer> result) {
+        if (root == null) {
+            return;
+        }
+        inorderTraversal(root.left, result);
+        result.add(root.val);
+        inorderTraversal(root.right, result);
+    }
+
+    private static void postorderTraversal(TreeNode root, List<Integer> result) {
+        if (root == null) {
+            return;
+        }
+        postorderTraversal(root.left, result);
+        postorderTraversal(root.right, result);
+        result.add(root.val);
+    }
+}
