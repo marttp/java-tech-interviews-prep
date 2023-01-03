63967cc1c2b9689ea8e8001b957dca4d2c9acff7
Thanaphoom Babparn <tp.babparn@gmail.com>
2023-01-03 12:04:03 +0900

    Adding search in BST

---
+++ src/lc/pattern/treedfs/SearchInBST_700.java
@@ -0,0 +1,19 @@
+package lc.pattern.treedfs;
+
+import common.TreeNode;
+
+public class SearchInBST_700 {
+
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null) {
+            return null;
+        }
+        if (root.val == val) {
+            return root;
+        } else if (root.val > val) {
+            return searchBST(root.left, val);
+        } else {
+            return searchBST(root.right, val);
+        }
+    }
+}
