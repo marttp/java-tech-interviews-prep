f0d952ef9d22156764dda8893c2a8d0d3841501e
Thanaphoom Babparn <tp.babparn@gmail.com>
2023-01-03 12:09:55 +0900

    Adding BFS Graph

--- src/datastructurealgorithms/graph/GraphExample.java
+++ src/datastructurealgorithms/graph/GraphExample.java
@@ -14,18 +14,43 @@ public class GraphExample {
         return graph;
     }
 
-    public static boolean dfs(int u, int destination, Map<Integer, List<Integer>> graph,
+    public static boolean dfs(int source, int destination, Map<Integer, List<Integer>> graph,
                                  Set<Integer> visited) {
-        if (u == destination) {
+        if (source == destination) {
             return true;
         }
-        for (int v : graph.get(u)) {
+        for (int v : graph.get(source)) {
             if (!visited.add(v)) {
                 if (dfs(v, destination, graph, visited)) {
                     return true;
                 }
             }
         }
+        return false;
+    }
+
+    public static boolean bfs(Map<Integer, List<Integer>> graph, Set<Integer> visited, int start, int end) {
+        if (start == end) {
+            return true;
+        }
+
+        Set<Integer> seen = new HashSet<>();
+        LinkedList<Integer> queue = new LinkedList<>();
+        queue.addAll(graph.get(start));
+
+        while (!queue.isEmpty()) {
+            int node = queue.poll();
+            if (node == end) {
+                return true;
+            }
+            for (int child : graph.get(node)) {
+                if (!seen.contains(child)) {
+                    queue.add(child);
+                    seen.add(child);
+                }
+            }
+        }
+
         return false;
     }
 }
