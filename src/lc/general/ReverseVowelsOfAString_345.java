6cc0fbda64f7ee2dacfb35a91c492fb6b030442b
Thanaphoom Babparn <tp.babparn@gmail.com>
2025-01-26 12:30:41 +0900

    Add 345. Reverse Vowels of a String

---
+++ src/lc/general/ReverseVowelsOfAString_345.java
@@ -0,0 +1,30 @@
+package lc.general;
+
+public class ReverseVowelsOfAString_345 {
+
+    public String reverseVowels(String s) {
+        char[] chars = s.toCharArray();
+        int left = 0;
+        int right = chars.length - 1;
+        while (left < right) {
+            while (left < right && !isVowel(chars[left])) {
+                left++;
+            }
+            while (left < right && !isVowel(chars[right])) {
+                right--;
+            }
+            if (isVowel(chars[left]) && isVowel(chars[right])) {
+                char tmp = chars[left];
+                chars[left++] = chars[right];
+                chars[right--] = tmp;
+            }
+        }
+        return String.valueOf(chars);
+    }
+
+    private boolean isVowel(char c) {
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' ||
+                c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U';
+    }
+
+}
