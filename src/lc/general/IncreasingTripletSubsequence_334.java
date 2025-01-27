29662a5eb1a048595834b16f6f84337e58332087
Thanaphoom Babparn <tp.babparn@gmail.com>
2025-01-27 20:57:14 +0900

    Add 334. Increasing Triplet Subsequence

---
+++ src/lc/general/IncreasingTripletSubsequence_334.java
@@ -0,0 +1,35 @@
+package lc.general;
+
+public class IncreasingTripletSubsequence_334 {
+
+    // O(n^3)
+    public boolean naiveSolution(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                for (int k = j + 1; k < nums.length; k++) {
+                    if (nums[i] < nums[j] && nums[j] < nums[k]) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    // O(n) - WTF moment
+    public boolean increasingTriplet(int[] nums) {
+        int first = Integer.MAX_VALUE;
+        int second = Integer.MAX_VALUE;
+        for (int n : nums) {
+            if (n <= first) {
+                first = n;
+            } else if (n <= second) {
+                second = n;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
