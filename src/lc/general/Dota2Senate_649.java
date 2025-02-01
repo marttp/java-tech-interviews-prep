5d87c9e882a1f7c6b0ce5d467a98f89254f27acd
Thanaphoom Babparn <tp.babparn@gmail.com>
2025-02-01 10:09:12 +0900

    Add 649. Dota2 Senate

---
+++ src/lc/general/Dota2Senate_649.java
@@ -0,0 +1,33 @@
+package lc.general;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Dota2Senate_649 {
+
+    public String predictPartyVictory(String senate) {
+        int n = senate.length();
+        Queue<Integer> radiantQueue = new LinkedList<>();
+        Queue<Integer> direQueue = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            char c = senate.charAt(i);
+            if (c == 'R') {
+                radiantQueue.add(i);
+            } else {
+                direQueue.add(i);
+            }
+        }
+        while (!radiantQueue.isEmpty() && !direQueue.isEmpty()) {
+            var r = radiantQueue.poll();
+            var d = direQueue.poll();
+            if (r < d) {
+                // Ban D => Put itself to queue
+                // But want to ignore next compare as well
+                radiantQueue.add(r + n);
+            } else {
+                direQueue.add(d + n);
+            }
+        }
+        return !radiantQueue.isEmpty() ? "Radiant" : "Dire";
+    }
+}
