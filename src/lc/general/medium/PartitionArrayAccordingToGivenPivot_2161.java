5d186b27153b976f4af8a58865d280531ba2224a
Thanaphoom Babparn <tp.babparn@gmail.com>
2025-03-04 07:02:45 +0900

    Add 2161. Partition Array According to Given Pivot

---
+++ src/lc/general/medium/PartitionArrayAccordingToGivenPivot_2161.java
@@ -0,0 +1,39 @@
+package lc.general.medium;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class PartitionArrayAccordingToGivenPivot_2161 {
+
+    class Solution1 {
+        public int[] pivotArray(int[] nums, int pivot) {
+            Queue<Integer>[] bucket = new Queue[2];
+            int pivotCounter = 0;
+            bucket[0] = new LinkedList<>();
+            bucket[1] = new LinkedList<>();
+
+            for (int n : nums) {
+                if (n == pivot) {
+                    pivotCounter++;
+                } else {
+                    int index = n > pivot ? 1 : 0;
+                    bucket[index].add(n);
+                }
+            }
+
+            int i = 0;
+            while (!bucket[0].isEmpty()) {
+                nums[i++] = bucket[0].poll();
+            }
+            while (pivotCounter-- > 0) {
+                nums[i++] = pivot;
+            }
+            while (!bucket[1].isEmpty()) {
+                nums[i++] = bucket[1].poll();
+            }
+
+            return nums;
+        }
+    }
+    
+}
