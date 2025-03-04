093119fdf4e8626af041e530415a620e0ccaf1be
Thanaphoom Babparn <tp.babparn@gmail.com>
2025-03-04 16:01:21 +0900

    Add 1780. Check if Number is a Sum of Powers of Three

---
+++ src/lc/general/medium/CheckIfNumberIsASumOfPowersOfThree_1780.java
@@ -0,0 +1,13 @@
+package lc.general.medium;
+
+public class CheckIfNumberIsASumOfPowersOfThree_1780 {
+    public boolean checkPowersOfThree(int n) {
+        while (n != 0) {
+            if (n % 3 == 2) {
+                return false;
+            }
+            n /= 3;
+        }
+        return true;
+    }
+}
